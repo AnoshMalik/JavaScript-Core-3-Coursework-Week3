05611a79ab523c688760c4249271967231ae4b8c
Anosh Malik <anoshmalik.am@gmail.com>
2022-11-16 15:48:31 +0000

    Finished exercise 3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,25 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+console.log(`QTY \t ITEM \t\t  TOTAL`);
+for (let x of order) {
+  if (x.itemName.length < 16) {
+    console.log(
+      `${x.quantity} \t ${x.itemName} \t  ${(x.quantity * x.unitPrice).toFixed(
+        2
+      )}`
+    );
+  } else {
+    console.log(
+      `${x.quantity} \t ${x.itemName} ${(x.quantity * x.unitPrice).toFixed(2)}`
+    );
+  }
+}
+
+let totalValue = 0;
+
+for (let x of order) {
+  totalValue += (x.quantity * x.unitPrice);
+}
+console.log(`\nTotal: ${totalValue}`);
